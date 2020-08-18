09be82641c498666978881a5b9fcde6759c0fb6e
pat <8444617+pnoll1@users.noreply.github.com>
2020-08-18 00:51:23 -0700

    added and activated slice function, added qa prep, quality check and filter data functions

--- osmand_osm/osm/processing.py
+++ osmand_osm/osm/processing.py
@@ -12,6 +12,7 @@ from subprocess import run, CalledProcessError
 from pathlib import Path
 import argparse
 from multiprocessing import Pool
+import shutil
 
 # commandline argument setup
 parser = argparse.ArgumentParser(description='Process address data to a single osm file per state')
@@ -151,10 +152,10 @@ def update_osm(state, state_expander):
     return
 
 
-def merge(state, master_list, state_expander, pbf_output):
+def merge(state, master_list, state_expander):
     '''
-    input: state as 2 letter abbrev., dict of sources to be merged, dict to expand state abbrev. to full name, output folder
-    output: merged state pbf in output folder
+    input: state as 2 letter abbrev., dict of sources to be merged, dict to expand state abbrev. to full name
+    output: merged state pbf in state folder
     '''
     list_files_string = []
     file_list = master_list.get(state)
@@ -164,7 +165,7 @@ def merge(state, master_list, state_expander, pbf_output):
     state_expanded = state_expander.get(state)
     state_expanded = state_expanded.replace(' ', '-')
     try:
-        run('osmium merge -Of pbf {0} {1}/{2}-latest.osm.pbf -o {3}/Us_{2}_northamerica_alpha.osm.pbf'.format(file_list_string, state, state_expanded, pbf_output), shell=True, capture_output=True, check=True, encoding='utf8')
+        run('osmium merge -Of pbf {0} {1}/{2}-latest.osm.pbf -o {1}/Us_{2}_northamerica_alpha.osm.pbf'.format(file_list_string, state, state_expanded), shell=True, capture_output=True, check=True, encoding='utf8')
     except Exception as e:
         print(e.stderr)
         print(state + ' ' + 'Merge Failed')
@@ -172,15 +173,78 @@ def merge(state, master_list, state_expander, pbf_output):
     print(state + ' ' + 'Merge Finished')
     return
 
-# region slicing
-# states above ~200MB can crash osmand map generator, slice into smaller regions before processing
-# define polyfiles or coordinates for slicing
-# osmium extract
+def prep_for_qa(state, state_expander, master_list):
+    '''
+    input: state abbrev, state_expander dict, master_list
+    output: stats for last source ran, state extract and final file
+    '''
+    state_expanded = state_expander.get(state)
+    # osmium sort runs everything in memory, may want to use osmosis instead
+    run('osmium sort --overwrite {0}/Us_{1}_northamerica_improved.osm.pbf -o {0}/Us_{1}_northamerica_alpha.osm.pbf'.format(state, state_expanded), shell=True, encoding='utf8')
+    # find last source ran
+    file_list = master_list.get(state)
+    last_source = Path(file_list[-1]).with_suffix('.osm')
+    # get data for last source ran
+    stats = run('osmium fileinfo -ej {0}'.format(last_source), shell=True, capture_output=True, encoding='utf8')
+    # get data for OSM extract
+    stats_state = run('osmium fileinfo -ej {0}-latest.osm.pbf'.format(state_expanded), shell=True, capture_output=True, encoding='utf8')
+    # get data for completed state file
+    stats_final = run('osmium fileinfo -ej Us_{0}_northamerica_alpha.osm.pbf'.format(state_expanded), shell=True, capture_output=True, encoding='utf8')
+    return stats, stats_state, stats_final
 
-# move files into osm directory defined in batch.xml
+def quality_check(stats, stats_state, stats_final):
+    '''
+    input: stats for last source ran, state extract and final file
+    output: boolean that is True for no issues or False for issues
+    '''
+    # file is not empty
+    # Check if items have unique ids
+    if json.loads(stats_final.stdout)['data']['multiple_versions'] == 'True':
+        print('ERROR: Multiple items with same id')
+        ready_to_move = False
+    # Check if added data overlaps with OSM ids
+    if json.loads(stats_state.stdout)['data']['maxid']['nodes'] >= json.loads(stats.stdout)['data']['minid']['nodes']:
+        print('ERROR: Added data overlaps with OSM data')
+        ready_to_move = False
+    return ready_to_move
+
+def move(state, state_expander, ready_to_move, pbf_output):
+    '''
+    input: state abbrev, state_expander dict, ready_to_move boolean, pbf_output location
+    output: nothing
+    '''
+    state_expanded = state_expander.get(state)
+    if ready_to_move:
+        shutil.move('{0}/Us_{1}_northamerica_alpha.osm.pbf'.format(state, state_expanded), pbf_output)
+
+def filter_data(state, master_list):
+    file_list = master_list.get(state)
+    for j in file_list:
+        # check for unicode control characters
+        r = run('psql -d gis -c "select pg_typeof({0}) from \\"{1}_{2}\\" limit 1;"'.format(number_field, name, state), shell=True, capture_output=True, encoding='utf8')
+
+
+def slice(state, state_expander):
+    '''
+    input: state, state_expander, (name of slice and bounding box coordinates in lon,lat,lon,lat)
+    output: sliced files name according to config
+
+    # states above ~200MB can crash osmand map generator, slice into smaller regions before processing
+    '''
+    config = {}
+    config['colorado'] = [['north', '-109.11,39.13,-102.05,41.00'], ['south', '-109.11,39.13,-102.04,36.99']]
+    config['florida'] = [['north', '-79.75,27.079,-87.759,31.171'], ['south', '79.508,24.237,-82.579,27.079']]
+    state_expanded = state_expander.get(state)
+    for state in config.keys():
+        for slice_config in config[state]:
+            # better as dict?
+            slice_name = slice_config[0]
+            bounding_box = slice_config[1]
+            run('osmium extract -b {3} -o {0}/Us_{1}_{2}_northamerica_alpha.osm.pbf {0}/Us_{1}_northamerica_alpha.osm.pbf'.format(state, state_expanded, slice_name, bounding_box), shell=True, capture_output=True, encoding='utf8')
 
 # run osmand map creator
 # batch.xml needs to be setup
+# move files into osm directory defined in batch.xml
 # java -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx6300M -cp "./OsmAndMapCreator.jar:lib/OsmAnd-core.jar:./lib/*.jar" net.osmand.util.IndexBatchCreator batch.xml
 
 
@@ -196,14 +260,20 @@ def run_all(state):
     master_list = create_master_list(state, master_list, oa_root)
     if args.load_oa == True:
         load_oa(state, master_list)
+    # filter_data(state, master_list)
     if args.output_osm == True:
         master_list = output_osm(state, master_list, id, root)
     if args.update_osm == True:
         update_osm(state, state_expander)
-    merge(state, master_list, state_expander, pbf_output)
-
+    merge(state, master_list, state_expander)
+    # prep_for_qa(state, state_expander, pbf_output)
+    ready_to_move = True
+    # ready_to_move = quality_check(state, master_list)
+    slice(state, state_expander)
+    move(state, state_expander, ready_to_move, pbf_output) 
 
 if __name__ == '__main__':
+    # memory can be limit with large files, consider switching pool to 1 or doing 1 state at a time with cron job
     with Pool(2) as p:
         if args.update_oa == True:
             oa_urls = ['https://data.openaddresses.io/openaddr-collected-us_midwest.zip', 'https://data.openaddresses.io/openaddr-collected-us_south.zip', 'https://data.openaddresses.io/openaddr-collected-us_west.zip', 'https://data.openaddresses.io/openaddr-collected-us_northeast.zip']
