2ea4138d31352338db5b9b5ac3e155be3bf2039d
User <8444617+pnoll1@users.noreply.github.com>
2022-12-13 12:26:50 +0000

    fix update script

--- osmand_osm/osm/update_app.sh
+++ osmand_osm/osm/update_app.sh
@@ -2,13 +2,16 @@
 
 install_dir=/home/pat/projects/osmand_map_creation
 # update dependencies
-sudo apt-get update
-sudo apt-get install -y curl osmium-tool postgresql postgis gdal-bin wget git python3-pip unzip
+sudo apt-get update 
+sudo apt-get upgrade -y
 sudo pip3 install -U ogr2osm psycopg2-binary
 # update code
 git pull
 # Download OsmAnd Map Creator
+cd $install_dir
+# remove old file
+rm OsmAndMapCreator-main.zip
 wget http://download.osmand.net/latest-night-build/OsmAndMapCreator-main.zip
 unzip -o OsmAndMapCreator-main.zip
-cd $install_dir
+# restore settings
 cp batch.xml.template batch.xml
