3eaede4dcdc45d9bba66a09ba91dfd57e1fb9185
User <8444617+pnoll1@users.noreply.github.com>
2023-06-25 02:14:57 +0000

    switch to bookworm

--- osmand_osm/osm/setup_container.sh
+++ osmand_osm/osm/setup_container.sh
@@ -6,6 +6,6 @@
 # lxc.idmap = g 0 232072 65536
 # if using btrfs backin add --bdev btrfs before --
 # setup container
-sudo lxc-create -q osmand_map_creation -t download -- -d debian -r sid -a amd64
+sudo lxc-create -q osmand_map_creation -t download -- -d debian -r bookworm -a amd64
 sudo lxc-start osmand_map_creation
 sudo cp setup_app.sh /var/lib/lxc/osmand_map_creation/rootfs/
