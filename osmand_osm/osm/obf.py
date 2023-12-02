c5e7367b750a1260111e45a2150bbff85afe3410
User <8444617+pnoll1@users.noreply.github.com>
2023-12-02 06:11:00 +0000

    inital obf.py cleanup

--- osmand_osm/osm/obf.py
+++ osmand_osm/osm/obf.py
@@ -1,4 +1,11 @@
-from subprocess import run
+'''
+utilites for working with Osmand's obf format
+'''
+import hashlib
+import logging
+import os
+from pathlib import Path
+from subprocess import run, CalledProcessError
 from config import Xmx
 
 def clean_file_names():
@@ -10,22 +17,29 @@ def clean_file_names():
             directory = file.parent
             new_filename = file.name.replace('_2','')
             new_file_path = directory.joinpath(Path(new_filename))
-            os.replace(file, new_file_path) 
+            os.replace(file, new_file_path)
 
-def build():
+def build(area_list):
     '''
-    runs osmand map creator, cleans obf file names and moves pbfs out of build folder
+    input: area list
+    action: runs osmand map creator on files in osmand_osm directory, cleans obf file names and moves pbfs into osm directory
+    output: none
     '''
-        logging.info('Builds started')
-        try:
-            run(f'cd ../..;java -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx{Xmx} -cp "./OsmAndMapCreator.jar:lib/OsmAnd-core.jar:./lib/*.jar" net.osmand.util.IndexBatchCreator batch.xml', shell=True, capture_output=True, check=True,encoding='utf8')
-        except CalledProcessError as error:
-            logging.error(str(area_list) + ' OsmAndMapCreator Failure, check osmand_gen/AREA_NAME_2.obf.gen.log file for details', exc_info = True)
-        # move files out of build folder
-        run('cd ..;mv *.pbf osm/', shell=True, capture_output=True, encoding='utf8')
-        clean_file_names()
-        
+    logging.info('Builds started')
+    try:
+        run(f'cd ../..;java -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx{Xmx} -cp "./OsmAndMapCreator.jar:lib/OsmAnd-core.jar:./lib/*.jar" net.osmand.util.IndexBatchCreator batch.xml', shell=True, capture_output=True, check=True,encoding='utf8')
+    except CalledProcessError as error:
+        logging.error(str(area_list) + ' OsmAndMapCreator Failure, check osmand_gen/AREA_NAME_2.obf.gen.log file for details', exc_info = True)
+    # move files out of build folder
+    run('cd ..;mv *.pbf osm/', shell=True, capture_output=True, encoding='utf8')
+    clean_file_names()
+
 def calculate_hashes():
+    '''
+    input: none
+    action: create hash file for every obf file in osmand_obf
+    output: none
+    '''
     for file in Path('../../osmand_obf').iterdir():
         if file.suffix == '.obf':
             with open(file, 'rb') as opened_file:
